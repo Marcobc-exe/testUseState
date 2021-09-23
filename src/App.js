13cd8d3dd251000404560a0a41f2acbb58ff789f
Marcobc.code <xmarcobravox@gmail.com>
2021-09-23 11:56:27 -0300

    Practicando con useState de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Note } from './Note';
+import { useState } from 'react';
 
-function App() {
+const App = (props) => {
+  // Validación: if (typeof notes === "undefined" || notes.length === 0) return "No tenemos notas que mostrar"
+  const [notes,setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState("")
+  
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const handleClick = (event) => {
+    console.log("crear nota")
+    const noteToAddToState = {
+      id: notes.length +1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+    console.log(noteToAddToState)
+    setNotes(notes.concat(noteToAddToState))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Notes</h1>
+      <ol>
+        {notes.map(note => <Note key={note.id} {...note} />)}
+      </ol>
+      <input type='text' placeholder='Escribir aquí' onChange={handleChange} />
+      <button onClick={handleClick}>Crear nota</button>
     </div>
   );
 }
---
+++ src/Note.js
@@ -0,0 +1,11 @@
+export const Note = (props) => {
+    const { content, date } = props
+    return (
+        <li>
+          <p>{content}</p>
+          <small>
+              <time>{date}</time>
+          </small>
+        </li>
+    )
+}
